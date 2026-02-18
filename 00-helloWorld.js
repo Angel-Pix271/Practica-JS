dca79814751cfc5c4602cd6c7ad240919060ae10
Angel-Pix271 <miguelangelmolinadiaz123@gmail.com>
2026-02-18 12:14:06 -0500

    chore: initial JavaScript practice - console and operators
    Includes basic console.log examples:
    - String output using single, double quotes and template literals
    - Basic arithmetic operations
    - Difference between string and number types

---
+++ 00-helloWorld.js
@@ -0,0 +1,48 @@
+// Esto es un comentario
+/**
+ * Esto es un comentario
+ * en varias lineas
+ * de texto
+ */
+
+// Imprime un texto usando comillas dobles
+console.log("Hola, JavaScript")
+
+// Imprime un texto usando comillas simples
+console.log('Hola, JavaScript')
+
+// Imprime un texto usando template literals (backticks)
+// Permite interpolar variables con ${}
+console.log(`Hola, JavaScript`)
+
+// Aqui se comentan para su no uso o uso despues
+/**
+console.log("Hola, JavaScript")
+console.log('Hola, JavaScript')
+console.log(`Hola, JavaScript`)
+*/
+
+// Imprime el número 5 como STRING (texto)
+console.log("5")
+
+// Imprime el número 5 como NUMBER (dato numérico)
+console.log(5)
+
+// Suma
+console.log(5 + 2)
+
+// Resta
+console.log(5 - 2)
+
+// División
+console.log(5 / 2)
+
+// Multiplicación
+console.log(5 * 2)
+
+// Módulo (residuo de la división)
+console.log(5 % 2)
+
+// Exponente (5 elevado a la 2)
+console.log(5 ** 2)
+
